f73d236840dad512da130dc81a8a9aa40dc7f77d
경 <106065973+developernagk@users.noreply.github.com>
2024-01-31 12:48:01 +0900

    Add files via upload

---
+++ javaBasic/src/김나경_프로그래밍언어응용/PassengerTrainVehicle.java
@@ -0,0 +1,8 @@
+package 김나경_프로그래밍언어응용;
+
+public interface PassengerTrainVehicle {
+	public static final String accelerate = "여객 열차가 천천히 가속합니다!";
+	public static final String stop = "여객 열차가 천천히 정차합니다!";
+	public static final boolean setStart = false;
+
+}

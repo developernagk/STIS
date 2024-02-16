714717c046652145dd2e3ba6a429e410c1df2bbe
developernagk <nagk907@gmail.com>
2024-02-16 14:25:26 +0900

    Completed merge

--- realEstate/src/com/nagyeong/realEstate/repository/EmailAuthenticationRepositiry.java
+++
@@ -1,9 +0,0 @@
-package com.nagyeong.realEstate.repository;
-
-import java.sql.SQLException;
-
-public interface EmailAuthenticationRepositiry {
-	boolean existByEmail(String email) throws SQLException;
-	
-	void save(String email, String authenticationCode) throws SQLException;
-}
--- realEstate/src/com/nagyeong/realEstate/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nagyeong.realEstate.repository;
-
-import java.sql.SQLException;
-
-public interface UserRepository {
-	boolean existsById(String id) throws SQLException;
-}

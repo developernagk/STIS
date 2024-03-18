43cca4fa48df000648f17f7ab9dcd0ee2ecf06c9
developernagk <nagk907@gmail.com>
2024-03-18 17:27:44 +0900

    240318 17:27 Springboot update

--- springworkspace/gradle/app/src/main/java/springworkspace/App.java
+++ springworkspace/gradle/app/src/main/java/springworkspace/App.java
@@ -3,8 +3,23 @@
  */
 package springworkspace;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 @Getter
+@Setter
+// @NoArgsConstructor: 변수 선언 시 final로 선언하면 무조건 값이 필요하기 때문에 빈 생성자 생성 불가능
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
 class Person {
+
+    private final String name;
+    private int age;
 
 }
 
@@ -15,5 +30,11 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        Person person = new Person("홍길동", 20);
+        person = new Person("고길동");
+
+        person = Person.builder().name("홍길동").age(20).build();
+
     }
 }

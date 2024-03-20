7bb402c0bacb752a723f1c3f4331c5c9e325575d
developernagk <nagk907@gmail.com>
2024-03-20 15:53:11 +0900

    240320 15:53 Springboot update

--- springworkspace/basic/src/main/java/com/nagyeong/basic/controller/RequestDataController.java
+++ springworkspace/basic/src/main/java/com/nagyeong/basic/controller/RequestDataController.java
@@ -1,9 +1,13 @@
 package com.nagyeong.basic.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.FlashMapManager;
 
 @RestController
 // HTTP * localhost:4000/request-data/**
@@ -15,12 +19,50 @@ public class RequestDataController {
     
     // HTTP GET localhost:4000/request-data/request-param
     @GetMapping("/request-param")
-    // http://localhost:4000/request-data/request-param?userId=qwer&userName=gildong
+    // http://localhost:4000/request-data/request-param?userId=qwer&userName=gildong&userAge=10
     public String getRequestParam(
         @RequestParam(name="userId") String userId,
-        @RequestParam(name="userName") String userName
+        @RequestParam(name="userName", required=false) String userName,
+        @RequestParam() int userAge
     ) {
-        return "사용자 아이디 : " + userId + "/ 사용자 이름 : " + userName;
+        return "사용자 아이디 : " + userId + "/ 사용자 이름 : " + userName + "/ 사용자 나이 : " + userAge;
+    }
+
+    // @PathVariable() :
+    // - 모든 HTTP 메서드에서 URL의 특정 패턴에 따라서 데이터를 추출하는 방식
+
+    //HTTP DELETE localhost:4000/request-data/path-variable
+    @DeleteMapping("/path-variable/{age}")
+    // http://localhost:4000/request-data/path-variable/10
+    public String deletePathVariable(
+        // int로 작성할 경우 0이 들어오게 되는데 사용자가 0을 입력한 것인지 기본값이 들어온 건지 알 수 없게 됨
+        @PathVariable("age") Integer age
+    ) {
+        return "사용자 나이 : " + age;
+    }
+
+    // HTTP PATCH localhost:4000/request-data/patch/gildong/update
+    @PatchMapping("/patch/{userName}/update")
+    public String patchUpdate(
+        @PathVariable("userName") String userName
+    ) {
+        return "사용자 이름 : " + userName;
+    }
+
+    //!***** 주의
+    // URL 패턴으로 데이터를 받아오는 방식을 썼을 때 겹치는 패턴이 존재하는지 잘 확인해야 함
+    // /{value}/get 와 /get/{value} 에서 value 이름이 같을 경우 error code: 500 발생
+    @GetMapping("/{value}/get")
+    public String getPathVariable1(
+        @PathVariable("value") String value
+    ) {
+        return "getPathVariable1";
+    }
+    @GetMapping("/get/{value}")
+    public String getPathVariable2(
+        @PathVariable("value") String value
+    ) {
+        return "getPathVariable2";
     }
 
 }

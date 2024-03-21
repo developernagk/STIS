4ba1b320a9b6a512e611eb8c277adfe5d1330ca2
developernagk <nagk907@gmail.com>
2024-03-21 10:47:20 +0900

    240321 10:47 Javascript object update

--- webworkspace/js/11.Object/object.js
+++ webworkspace/js/11.Object/object.js
@@ -91,39 +91,82 @@ console.log('==============================');
 
 /*
     속성 값 읽기 : 
-    - 
-    - 
-    - 
-    - 
+    - . 표기법과 [] 표기법으로 접근 가능
+    - . 표기법은 유효한 이름일 때 사용
+    - [] 표기법은 유효한 이름이 아닐 때와 접근할 속성이 동적으로 변할 때 사용([] 안에 반드시 문자열로 지정)
+    - 객체에 존재하지 않는 속성에 접근하면 undefined를 반환
 */
-
+console.log(kimNagyeong.age); // 출력: 24
+console.log(kimNagyeong["last-name"]); // 출력: kim
+let property = 'first name';
+console.log(kimNagyeong[property]); // 출력: nagyeong
 
 
 console.log('==============================');
 
 /*
     속성 값 변경 :
-    - 
+    - 객체 속성에 접근하여 새로운 값을 할당하여 변경 가능
+    - 만약 존재하지 않는 속성에 값을 할당하면 새로운 속성이 생성
 */
-
+goJong.name = '이형';
+goJong.birth = '1852-09-08';
+console.log(goJong); // 출력: { name: '이형', age: 40, address: '서울특별시', birth: '1852-09-08' }
 
 
 console.log('==============================');
 
 /*
     속성 삭제 :
-    - 
+    - delete 연산자를 사용하여 객체 속성 제거 가능
 */
+delete goJong.birth;
+console.log(goJong); // 출력:{ name: '이형', age: 40, address: '서울특별시' }
 
 
 console.log('==============================');
 
 /*
     for-in 문 : 
-    - 
-    - 
+    - for-in문으로 객체에 포함되어 있는 키를 순회해서 접근 가능
+    - 반환되는 값은 문자열 타입의 키가 반환
 */
-
+for (const key in goJong) {
+    console.log(`${key}:${goJong[key]}`);
+}
+// 출력:
+// name:이형
+// age:40
+// address:서울특별시
 
 
 console.log('==============================');
+
+let sunJong  = goJong; // 변수의 주소가 달라지기 때문에 같이 바뀌게 됨
+sunJong.name = '이척';
+console.log(sunJong.name); // 출력: 이척
+console.log(goJong.name); // 출력: 이척
+
+// {}로 객체를 새로 생성하는 경우 주소가 새로 생기며 할당되기 때문에 goJong과 sunJong의 위치가 달라짐
+sunJong = {
+    name: goJong.name,
+    age: goJong.age,
+    address: goJong.address
+};
+
+sunJong = {};
+for (const key in goJong) {
+    sunJong[key] = goJong[key];
+}
+console.log(sunJong); // 출력: { name: '이척', age: 40, address: '서울특별시' }
+
+sunJong = {};
+console.log(sunJong); // 출력: {}
+
+// name, age, address라고 하는 각각의 변수로 바뀌며 배열이 할당됨
+sunJong = { ...goJong };
+console.log(sunJong); // 출력: { name: '이척', age: 40, address: '서울특별시' }
+
+goJong.name = '이형'; // goJong의 주소가 변경된 것이 아니라 goJong.name에 담긴 값이 변경된 것
+console.log(goJong.name); // 출력: 이형
+console.log(sunJong.name); // 출력: 이척

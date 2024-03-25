f519ff0e59d77f599d1ec266f5248f36d6fa5a3b
developernagk <nagk907@gmail.com>
2024-03-25 14:50:47 +0900

    240325 14:50 Springboot service update

---
+++ springworkspace/basic/src/main/java/com/nagyeong/basic/service/implement/BasicServiceImplement.java
@@ -0,0 +1,12 @@
+package com.nagyeong.basic.service.implement;
+
+import com.nagyeong.basic.service.BasicService;
+
+public class BasicServiceImplement implements BasicService {
+
+    @Override
+    public String getHello() {
+        return "Hello Springboot!!";
+    }
+    
+}

c20bfb161479856b9b40df8044961e5061d9714c
developernagk <nagk907@gmail.com>
2024-03-25 16:16:48 +0900

    240325 16:16 Springboot service update

--- springworkspace/basic/src/main/java/com/nagyeong/basic/controller/BasicController.java
+++ springworkspace/basic/src/main/java/com/nagyeong/basic/controller/BasicController.java
@@ -26,17 +26,30 @@ import lombok.RequiredArgsConstructor;
 // @RequestMapping(value="/main", method={RequestMethod.GET})
 // HTTP GET localhost:4000/main/**
 @RequestMapping("/main")
+// final로 선언된 필드 변수에 대해서 생성자를 자동으로 작성함
 @RequiredArgsConstructor
 public class BasicController {
     
     // private BasicService service;
 
+    // 의존성 역전(DI) : 
+    // - 해당 클래스에 필요한 의존성을 외부(클래스의 인스턴스를 생성하는 위치)에서 주입
+    // - 생성자를 통한 의존성 주입, setter 메서드를 통한 의존성 주입, 필드를 통한 의존성 주입
+    // - 생성자를 통한 의존성 주입을 Spring에서 권장(의존성이 주입되지 않은 경우가 존재할 수 없기 때문)
+    // - Spring Framework에서 의존성 주입은 제어의 역전(IoC)를 통해서 Spring Framework가 진행(@Component를 사용해서 Spring Bean으로 등록된 Class에 한해서)
+
+    // @Autowired : 등록된 Spring Bean을 제어의 역전을 통해 의존성을 주입하는 방법
+    // - 단, 생성자를 통한 의존성 주입에는 @Autowired를 생략해도 됨
+
+    // @Autowired
     // @RestController를 쓰면 클래스를 생성함과 동시에 생성자를 만듦
-    // @Autowired : 생성자를 쓰면 안 적어줘도 됨(적는 걸 권장)
     // public BasicController(BasicService service) {
     //     this.service = service;
     // }
 
+    // 의존성 주입 시 클래스로 직접 참조변수를 만들지 않고 인터페이스로 간접적으로 만드는 이유
+    // : 고수준의 모듈에서 저수준의 모듈을 직접 참조하지 않고 추상화를 통해 간접 참조함으로써 각 모듈 간의 결합도를 낮춤
+    // -> 코드의 재사용성 향상, 유지보수성 향상
     // 상수(초기화 필수)로 선언된 것으로 변경 불가능(setter 사용 불가능)
     private final BasicService service;
 

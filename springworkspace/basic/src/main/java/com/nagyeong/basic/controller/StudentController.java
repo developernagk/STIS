3a9ad679395e22a118a62b8c6c0643d53790c33d
developernagk <nagk907@gmail.com>
2024-03-27 14:49:48 +0900

    240327 14:49 Springboot JPA update

---
+++ springworkspace/basic/src/main/java/com/nagyeong/basic/controller/StudentController.java
@@ -0,0 +1,17 @@
+package com.nagyeong.basic.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+  @PostMapping("/")
+  public ResponseEntity<?> postStudent() {
+    return null;
+  }
+  
+}

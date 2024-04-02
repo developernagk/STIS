878f6c9cc03fb23f0ede4dc602e897957be6d4bd
developernagk <nagk907@gmail.com>
2024-04-02 09:50:24 +0900

    240402 9:50 TypeScript update

--- webworkspace/ts/2.type/type.ts
+++ webworkspace/ts/2.type/type.ts
@@ -2,8 +2,16 @@
     타입선언 : 
     - 변수 및 함수에 타입을 지정하여 타입에 대한 제한을 추가할 수 있음
 */
+let variable: string = '문자열';
+// variable = true;
 
+function func (arg: number): string {
+    return arg + '';
+}
 
+const arrowFunc = (arg: number): string => {
+    return arg + '';
+}
 
 /*
     typescript의 타입

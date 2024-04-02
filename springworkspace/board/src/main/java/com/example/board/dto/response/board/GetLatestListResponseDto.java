cd1c12fb7e4b83b78666c30928492a033fe393f9
developernagk <nagk907@gmail.com>
2024-04-02 15:48:57 +0900

    240402 15:48 Springboot example update

--- springworkspace/board/src/main/java/com/example/board/dto/response/board/GetLatestListResponseDto.java
+++ springworkspace/board/src/main/java/com/example/board/dto/response/board/GetLatestListResponseDto.java
@@ -2,7 +2,12 @@ package com.example.board.dto.response.board;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.example.board.dto.response.ResponseCode;
 import com.example.board.dto.response.ResponseDto;
+import com.example.board.dto.response.ResponseMessage;
 import com.example.board.dto.response.board.item.BoardListItem;
 
 import lombok.Getter;
@@ -11,8 +16,14 @@ import lombok.Getter;
 public class GetLatestListResponseDto extends ResponseDto {
   private List<BoardListItem> latestList;
 
-  public GetLatestListResponseDto(String code, String message, List<BoardListItem> latestList) {
-    super(code, message);
+  private GetLatestListResponseDto(List<BoardListItem> latestList) {
+    super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
     this.latestList = latestList;
   }
+
+  public static ResponseEntity<GetLatestListResponseDto> success(List<BoardListItem> latestList) {
+    GetLatestListResponseDto body = new GetLatestListResponseDto(latestList);
+    return ResponseEntity.status(HttpStatus.OK).body(body);
+  }
+
 }

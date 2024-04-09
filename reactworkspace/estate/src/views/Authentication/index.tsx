d88d1f0c512cfa7c492b6094d74986163e69ae03
developernagk <nagk907@gmail.com>
2024-04-09 12:50:18 +0900

    240409 12:50 React estate update

--- reactworkspace/estate/src/views/Authentication/index.tsx
+++ reactworkspace/estate/src/views/Authentication/index.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
-export default function Authentication() {
+type AuthPage = 'sign-in' | 'sign-up'
+
+function SignIn () {
+
+  const onSignInButtonClickHandler = () => {
+
+  };
+
   return (
-    <div>Authentication Page</div>
+    <div className="authentication-contents">
+      <div className="authentication-input-container"></div>
+      <div className="authentication-button-container">
+        <div className="primary-button full-width" onClick={onSignInButtonClickHandler}>로그인</div>
+        <div className="text-link">회원가입</div>
+      </div>
+      <div className="short-divider"></div>
+      <div className="authentication-sns-container"></div>
+    </div>
+  );
+}
+
+function SignUp () {
+  return (
+    <>회원가입</>
+  );
+}
+
+export default function Authentication() {
+
+  const [page, setPage] = useState<AuthPage>('sign-in');
+
+  const AutenticationContents = page === 'sign-in' ? <SignIn /> : <SignUp />
+
+  return (
+    <div id="authentication-wrapper">
+      <div className="authentication-image-box"></div>
+      <div className="authentication-box">
+        <div className="authentication-container">
+          <div className="authentication-title h1">{'임대주택 가격 서비스'}</div>
+          { AutenticationContents }
+        </div>
+      </div>
+    </div>
   )
 }

b0368f190f8daac061be378f0fcb4d2a32ce2ca4
developernagk <nagk907@gmail.com>
2024-04-11 11:49:30 +0900

    240411 11:49 React estate update

--- reactworkspace/estate/src/views/Authentication/index.tsx
+++ reactworkspace/estate/src/views/Authentication/index.tsx
@@ -7,6 +7,27 @@ import InputBox from 'components/Inputbox';
 
 type AuthPage = 'sign-in' | 'sign-up';
 
+interface SnsContainerProps {
+  title: string;
+}
+
+function SnsContainer({ title }: SnsContainerProps) {
+
+  const onSnsButtonClickHandler = (type: 'kakao' | 'naver') => {
+    alert(type);
+  };
+
+  return (
+    <div className="authentication-sns-container">
+      <div className="sns-container-title label">{title}</div>
+      <div className="sns-button-container">
+        <div className="sns-button kakao-button" onClick={() => onSnsButtonClickHandler('kakao')}></div>
+        <div className="sns-button naver-button" onClick={() => onSnsButtonClickHandler('naver')}></div>
+      </div>
+    </div>
+  );
+}
+
 interface Props {
   onLinkClickHandler: () => void;
 }
@@ -40,12 +61,21 @@ function SignIn ({ onLinkClickHandler }: Props) {
         <div className="text-link" onClick={onLinkClickHandler}>회원가입</div>
       </div>
       <div className="short-divider"></div>
-      <div className="authentication-sns-container"></div>
+      <SnsContainer title="SNS 로그인" />
     </div>
   );
 }
 
 function SignUp ({ onLinkClickHandler }: Props) {
+
+  const [id, setId] = useState<string>('');
+  const [idButtonStatus, setIdButtonStatus] = useState<boolean>(false);
+
+  const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setId(value);
+    setIdButtonStatus(value !== '');
+  };
 
   const onSignUpButtonClickHandler = () => {
 
@@ -53,9 +83,11 @@ function SignUp ({ onLinkClickHandler }: Props) {
 
   return (
     <div className="authentication-contents">
-      <div className="authentication-sns-container"></div>
+      <SnsContainer title="SNS 회원가입" />
       <div className="short-divider"></div>
-      <div className="authentication-input-container"></div>
+      <div className="authentication-input-container">
+        <InputBox label="아이디" type="text" value={id} placeholder="아이디를 입력해주세요." onChangeHandler={onIdChangeHandler} buttonTitle="중복 확인" buttonStatus={idButtonStatus} />
+      </div>
       <div className="authentication-button-container">
         <div className="primary-button full-width" onClick={onSignUpButtonClickHandler}>회원가입</div>
         <div className="text-link" onClick={onLinkClickHandler}>로그인</div>

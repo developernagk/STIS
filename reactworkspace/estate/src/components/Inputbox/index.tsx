7f7756637ec60b9c4f51eaf85a259e8997b24619
developernagk <nagk907@gmail.com>
2024-04-11 10:49:33 +0900

    240411 10:49 React estate update

--- reactworkspace/estate/src/components/Inputbox/index.tsx
+++ reactworkspace/estate/src/components/Inputbox/index.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './style.css';
 
 export interface InputBoxProps {
   label: string;
   type: 'text' | 'password';
+  value: string;
   placeholder: string;
-  check?: string;
+  onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+  buttonTitle?: string;
 }
 
-export default function InputBox({label, type, placeholder, check}: InputBoxProps) {
+export default function InputBox({label, type, value, placeholder, onChangeHandler, buttonTitle}: InputBoxProps) {
   return (
     <div className="input-box">
       <div className="input-label label">{label}</div>
       <div className="input-content-box">
-        <input className="input" type={type} placeholder={placeholder} />
-        { check && 
-        <div className="input-disable-button">{check}</div> }
+        <input className="input" type={type} value={value} placeholder={placeholder} onChange={onChangeHandler} />
+        { buttonTitle && 
+        <div className="input-disable-button">{buttonTitle}</div> }
       </div>
       <div className="input-message"></div>
     </div>

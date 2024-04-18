7aaaf9e9f83d33d5fee97443431272923d557bf3
developernagk <nagk907@gmail.com>
2024-04-18 11:51:56 +0900

    240418 11:51 React libraries update

--- reactworkspace/libraries/src/App.tsx
+++ reactworkspace/libraries/src/App.tsx
@@ -4,6 +4,7 @@ import ZustandLibrary from 'src/zustand/ZustandLibrary';
 import { Route, Routes } from 'react-router';
 import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
 import Layout from './react-router/Layout';
+import AxiosLibrary from './axios/AxiosLibrary';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path='page2' element={<ReactRouterPage2 />} />
       </Route>
       <Route path='/zustand' element={<ZustandLibrary />} />
+      <Route path='/axios' element={<AxiosLibrary />} />
       <Route path='*' element={<h3>404 Error</h3>} />
     </Routes>
   );

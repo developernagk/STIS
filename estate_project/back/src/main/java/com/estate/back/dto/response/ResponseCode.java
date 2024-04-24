3788f4a85372122f2eb5c882f541c3d326b4b428
developernagk <nagk907@gmail.com>
2024-04-24 14:54:41 +0900

    240424 14:54 Project estate back update

--- estate_project/back/src/main/java/com/estate/back/dto/response/ResponseCode.java
+++ estate_project/back/src/main/java/com/estate/back/dto/response/ResponseCode.java
@@ -2,5 +2,13 @@ package com.estate.back.dto.response;
 
 // Response의 공통된 code 값
 public interface ResponseCode {
-  
+  String SUCCESS = "SU";
+  String VALIDATION_FAILED = "VF";
+  String DUPLICATED_ID = "DI";
+  String DUPLICATED_EMAIL = "DE";
+  String SIGN_IN_FAILED = "SF";
+  String AUTHENTICATION_FAILED = "AF";
+  String TOKEN_CREATEION_FAILED = "TF";
+  String MAIL_SEND_FAILED = "MF";
+  String DATABASE_ERROR = "DBE";
 }

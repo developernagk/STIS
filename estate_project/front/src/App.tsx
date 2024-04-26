b2c9a61d971367c4a3e2014c24b4459ff105b7c0
developernagk <nagk907@gmail.com>
2024-04-26 10:52:22 +0900

    240426 10:52 Project estate front update

--- estate_project/front/src/App.tsx
+++ estate_project/front/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import { AUTH_PATH, LOCAL_PATH, QNA_DETAIL_PATH, QNA_PATH, QNA_UPDATE_PATH, QNA_WRITE_PATH, RATIO_PATH, SERVICE_PATH } from './constant';
 
 function App() {
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path={AUTH_PATH} element={<></>} />
+      <Route path={SERVICE_PATH} element={<></>}>
+        <Route path={LOCAL_PATH} element={<></>} />
+        <Route path={RATIO_PATH} element={<></>} />
+        <Route path={QNA_PATH} >
+          <Route index element={<></>} />
+          <Route path={QNA_WRITE_PATH} element={<></>} />
+          <Route path={QNA_DETAIL_PATH} element={<></>} />
+          <Route path={QNA_UPDATE_PATH} element={<></>} />
+        </Route>
+      </Route>
+      <Route path='*' element={<></>} />
+    </Routes>
   );
 }
 

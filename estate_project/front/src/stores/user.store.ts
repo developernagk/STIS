9b7e2a06dbfe5acd93cfeff641c836bd9f0c0e07
developernagk <nagk907@gmail.com>
2024-04-30 12:52:04 +0900

    240430 12:52 Project estate update

--- estate_project/front/src/stores/user.store.ts
+++ estate_project/front/src/stores/user.store.ts
@@ -1,13 +1,17 @@
 import { create } from "zustand";
 
 interface UserStore {
-  role: string,
-  setRole: (role: string) => void
+  loginUserId: string,
+  setLoginUserId: (loginUserId: string) => void,
+  loginUserRole: string,
+  setloginUserRole: (loginUserRole: string) => void
 }
 
 const useUserStore = create<UserStore>(set => ({
-  role: '',
-  setRole: (role: string) => set(state => ({ ...state, role }))
+  loginUserId: '',
+  setLoginUserId: (loginUserId: string) => set(state => ({ ...state, loginUserId })),
+  loginUserRole: '',
+  setloginUserRole: (loginUserRole: string) => set(state => ({ ...state, loginUserRole }))
 }));
 
 export default useUserStore;

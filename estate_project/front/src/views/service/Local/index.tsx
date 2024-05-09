0826ce78c9d24482748426de06ce721b0d9e30be
developernagk <nagk907@gmail.com>
2024-05-09 12:49:07 +0900

    240509 12:49 Project estate update

--- estate_project/front/src/views/service/Local/index.tsx
+++ estate_project/front/src/views/service/Local/index.tsx
@@ -1,16 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import SelectBox from 'src/components/Selectbox';
+import { CategoryScale, Chart as ChartJS, LineElement, LinearScale, PointElement, Tooltip } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip
+);
 
 //   component   //
 export default function Local() {
+  const option = {
+    responsive: true
+  };
+
+  //   state   //
+  const [selectLocal, setSelectLocal] = useState<string>('');
+
+  //   event handler   //
+  const onLocalChangeHandler = (selectLocal: string) => {
+    setSelectLocal(selectLocal);
+  };
+
+  const saleData = {
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+    datasets: [{
+      label: '매매 평균',
+      data: [225, 224, 224, 224, 200, 211, 225, 224, 224, 224, 200, 211],
+      borderColor: 'rgba(58, 87, 232, 1)',
+      backgroundColor: 'rgba(58, 87, 232, 1)'
+    }]
+  }
+
+  const leaseData = {
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+    datasets: [{
+      label: '전세 평균',
+      data: [225, 224, 224, 224, 200, 211, 225, 224, 224, 224, 200, 211],
+      borderColor: 'rgba(58, 87, 232, 1)',
+      backgroundColor: 'rgba(58, 87, 232, 1)'
+    }]
+  }
+
+  const monthRentData = {
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+    datasets: [{
+      label: '월세 평균',
+      data: [225, 224, 224, 224, 200, 211, 225, 224, 224, 224, 200, 211],
+      borderColor: 'rgba(58, 87, 232, 1)',
+      backgroundColor: 'rgba(58, 87, 232, 1)'
+    }]
+  }
 
   //   render   //
   return (
     <div id="local-wrapper">
       <div className="local-top">
         <div className="local-search-box">
-          <SelectBox />
+          <SelectBox value={selectLocal} onChange={onLocalChangeHandler} />
           <div className="primary-button">검색</div>
         </div>
         <div className="local-origin-text">데이터 출처: KOSIS</div>
@@ -21,7 +72,7 @@ export default function Local() {
           <div className="local-card-unit">(단위: 백만원)</div>
         </div>
         <div className="local-card-chart-box">
-
+          <Line options={option} data={saleData}/>
         </div>
       </div>
       <div className="local-card">
@@ -30,7 +81,7 @@ export default function Local() {
           <div className="local-card-unit">(단위: 백만원)</div>
         </div>
         <div className="local-card-chart-box">
-          
+          <Line options={option} data={leaseData}/>
         </div>
       </div>
       <div className="local-card">
@@ -39,9 +90,9 @@ export default function Local() {
           <div className="local-card-unit">(단위: 백만원)</div>
         </div>
         <div className="local-card-chart-box">
-          
+          <Line options={option} data={monthRentData}/>
         </div>
       </div>
     </div>
-  )
+  )  
 }
